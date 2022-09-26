cde958e274c5fc3107e593df9b288d3827c5e3c5
Drew <contact@dorrisd.com>
2022-09-26 14:40:37 -0400

    update with clarified requirements, make it runnable

--- Program.cs
+++ Program.cs
@@ -56,40 +56,42 @@ namespace Project1
 
         // Method which recursively finds result of last value of number added with same number without that digit
         public static int GetLastDigitAddedWithRestOfNum(int num) {
-            // This retrieves the last digit of he number
+            // This retrieves the last digit of the number
             int lastDigit = num % 10;
+            //Console.WriteLine(lastDigit); // debug
             // This is the inputted number with the last digit removed. Reduce it by a factor of 10 and remove the last number.
             // Example
             // 953 ---> (953 - 3) / 10 ---> 95
             num = (num - lastDigit) / 10;
-            // This would mean that the inputted number was a single digit or less than 10.
-            // Therefore we are done!
-            if (num <= 0) {
-                return lastDigit;
-            }
+            //Console.WriteLine(num); // debug
 
+            // Clarification was made that the last digit matches the sign of the rest of the number
+            // We do not change the sign of the last digit
             int added = lastDigit + num;
-            if (added < 10) {
+            if (added < 10 && added > -10) {
                 // It's a single digit, so we're done!
                 return added;
             }
-            Console.WriteLine("Currently at " + added);
+            Console.WriteLine("Currently at " + added); // this is for debug but displaying it anyway
             return GetLastDigitAddedWithRestOfNum(added);
         }
 
         // Unit test to verify correct result from normal values
         public static bool ReturnsCorrectResult() {
             // unimpl
+            return false;
         }
 
         // Unit test to verify input that would result in a negative result does
         public static bool CanReturnNegative() {
             // unimpl
+            return false;
         }
 
         // Only one digit inputted returns false
         public static bool ReturnsFromNotEnoughDigits() {
             // unimpl
+            return false;
         }
     }
 }
